4457be8923370b50dd1c1f356e3f56a853de934b
byron <byrondalberg@gmail.com>
2022-09-12 17:53:16 -0700

    new commit v2?

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://BihonAnon.github.io',
-  base: '/Astro-Portfolio-Byron-Dalberg',
 });
